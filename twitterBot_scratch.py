4e2fb217593b7706997c86a10edfcb3bb30e0709
Shivaye <shivaye241199modi@gmail.com>
2022-12-25 17:15:56 +0530

    Script will execute once only now

--- twitterBot_scratch.py
+++ twitterBot_scratch.py
@@ -22,27 +22,33 @@ def performQueryAndReply(old_id=None):
  api = tweepy.API(auth)
  query_string = "of 100DaysOfCode -filter:retweets"
  for tweet in tweepy.Cursor(api.search_tweets,query_string).items():
+     try:
 
-    # get tweet details
-  print("Tweet is from @", tweet.user.screen_name)
-  user_screen_name = tweet.user.screen_name
-  user_id = tweet._json["id"]
-   # tweet id must not repeat again
-  if tweet.id!=old_id:
-    old_id=tweet.id  
-    print(tweet.text)
+         # get tweet details
+         print("Tweet is from @", tweet.user.screen_name)
+         user_screen_name = tweet.user.screen_name
+         user_id = tweet._json["id"]
+         # tweet id must not repeat again
+         if tweet.id != old_id:
+             old_id = tweet.id
+             print(tweet.text)
 
-    # select a random quote
-    my_reply = random.choice(quotes.quotes)
+             # select a random quote
+             my_reply = random.choice(quotes.quotes)
 
-    # reply to tweet
-    api.update_status(my_reply, in_reply_to_status_id=tweet.id, auto_populate_reply_metadata=True)
-    print("Replied to tweet=", my_reply)
-    time.sleep(86400)
-    break
-  else:
-    continue  
- performQueryAndReply()
+             # reply to tweet
+             api.update_status(my_reply, in_reply_to_status_id=tweet.id, auto_populate_reply_metadata=True)
+             print("Replied to tweet=", my_reply)
+             # time.sleep(86400)
+             break
+         else:
+             continue
+
+     except tweepy.errors.TweepyException as exception :
+         print(exception)
+         continue
+
+     # performQueryAndReply()
 
 
 performQueryAndReply()
