ea2969873b3cff7ea72494ef922ad6f1543580b3
Shivaye Modi <99311839+ShivayeModi@users.noreply.github.com>
2024-07-14 01:03:38 +0530

    Creds fetched from env variables

--- credentials.py
+++ credentials.py
@@ -1,12 +1,10 @@
-# This is just a sample file. You need to
-# edit this file. You need to get these
-# details from your Twitter app settings.
+import os
 
-consumer_key = 'aVPFVxgK56DAkCLJaexCUi2Mz'
-consumer_secret = 'N3NrMy8Hdd2CVxZgvXE7iAXPiUOHL2qmHe9M33gFN2Cw0OdoVv'
-access_token = '1464491246257418253-AU4JgFsezgtlm6rr7TlwNaFuvcWjtt'
-access_token_secret = 'jlEfNlgy32nfUJC52lMUQUDobqjJtj47QAKulGAdcRQ2b'
+consumer_key = os.getenv('CONSUMER_KEY')
+consumer_secret = os.getenv('CONSUMER_SECRET')
+access_token = os.getenv('ACCESS_TOKEN')
+access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
 
-bearer_token= "AAAAAAAAAAAAAAAAAAAAAJn9oAEAAAAAH4TdnfR2i%2Bi5bN1bGZ7YtdKRq2Q%3Dmk1kLJkJvCkDrNOPr0dPG2hsvLSZ6NvzvpwQSAuFAxrofOdJzl"
+bearer_token= os.getenv('BEARER_TOKEN')
 
-gen_ai_api_key = "AIzaSyAjmotkCNQcDVeRFybs-oklou-iP4NkqWw"
+gen_ai_api_key = os.getenv('GEN_AI_API_KEY')
